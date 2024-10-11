743ab75bb0a908afc440440a6efda3bd711fd817
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-10-11 13:30:34 +0200

    feat: total value calculate automatically

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
 import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-let transactions: any = ref([
+const transactions: any = ref([
     { id: 1, name: 'Flower', amount: -19.99 },
     { id: 2, name: 'Salary', amount: 299.99 },
     { id: 3, name: 'Pudding', amount: -2.99 },
 ])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0)
+});
 </script>
 
 <template>
   <div class="container">
     <Header />
 
-    <Balance />
+    <Balance :total="total"/>
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    total: {
+        type: Number,
+        required: true,
+    }
+})
 </script>
 
 <template>
     <h4>Your balance</h4>
-    <h1 id="balance">$0.00</h1>
+    <h1 id="balance">${{ props.total }}</h1>
 </template>
 
 <style scoped>
